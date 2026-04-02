3408c6288142cddeb0eb3ce46a1b45fb4bb336db
mancron <iskra21d@gmail.com>
2026-04-02 12:32:35 +0900

    병함 오류수정

--- src/main/java/com/hrms/common/filter/AuthenticationFilter.java
+++ src/main/java/com/hrms/common/filter/AuthenticationFilter.java
@@ -66,9 +66,7 @@ public class AuthenticationFilter implements Filter {
             }
         }
         
-<<<<<<< examine
-        
-=======
+
         // 	4. 근태/휴가 관리자 기능 권한 체크
         if (path.startsWith("/att/leave/approve") 
                 || path.startsWith("/att/status") 
@@ -81,7 +79,6 @@ public class AuthenticationFilter implements Filter {
                 return;
             }
         }
->>>>>>> master
 
         // 모든 보안 검증을 통과한 정상 요청만 다음 필터나 서블릿으로 보낸다.
         chain.doFilter(request, response);
